979e7178702af1397d65179619bdf05d63f4f64c
julien345 <122797558+julien345@users.noreply.github.com>
2026-08-10 23:46:16 +0100

    Revert "Revert "Feature/order delivery""

--- src/modules/dish/dish.repository.ts
+++ src/modules/dish/dish.repository.ts
@@ -15,6 +15,12 @@ class DishRepository {
     });
   }
 
+  findManyByIds(ids: string[]) {
+    return prisma.dish.findMany({
+      where: { id: { in: ids }, deletedAt: null },
+    });
+  }
+
   findByCategory(categoryId: string) {
     return prisma.dish.findMany({
       where: { categoryId, deletedAt: null },
